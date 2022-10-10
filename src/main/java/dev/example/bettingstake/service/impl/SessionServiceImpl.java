994ae6a53010b2883d2320ac03ba8c81cf61578b
yeyisam <one_leaves@live.cn>
2022-10-10 18:26:11 +0800

    update

--- src/main/java/dev/example/bettingstake/service/impl/SessionServiceImpl.java
+++ src/main/java/dev/example/bettingstake/service/impl/SessionServiceImpl.java
@@ -1,13 +1,54 @@
 package dev.example.bettingstake.service.impl;
 
+import dev.example.bettingstake.model.Session;
 import dev.example.bettingstake.service.SessionService;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class SessionServiceImpl implements SessionService {
 
+    private  static Map<Integer, Session> customerSession=new ConcurrentHashMap<Integer,Session>();
+
     @Override
     public String getSessionKeyByCustomerId(Integer customerId) {
-        return null;
+        Session session=customerSession.get(customerId);
+
+        if(null==session){
+            session=GenerateNewSession(customerId);
+            customerSession.put(customerId.intValue(),session);
+        }
+        else{
+            Date expireTime= session.getExpireTime();
+            Date now=new Date();
+            if(expireTime.compareTo(now)>0){
+                return  session.getSessionKey();
+            }
+            else{
+                session.setExpireTime();
+                session.setSessionKey(GetGUID()+customerId);
+                customerSession.put(customerId.intValue(),session);
+            }
+        }
+
+        return session.getSessionKey();
+    }
+
+    private  Session GenerateNewSession(Integer customerId){
+        String sessionKey= GetGUID()+customerId;
+        Session session=new  Session();
+        session.setSessionKey(sessionKey);
+        session.setCustomerId(customerId);
+        session.setExpireTime();
+        return session;
+    }
+
+
+    private String GetGUID() {
+        return UUID.randomUUID().toString().replace("-", "");
     }
 }
